0e9c73809c248df22c90df49b5ad524ea3b66042
marcus2188 <marcus.2188@gmail.com>
2020-07-01 11:59:19 +0800

    refined files

--- Logreg_Confusion.py
+++ Logreg_Confusion.py
@@ -2,9 +2,12 @@
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
 import time as tm
 import pandas as pd
 import numpy as np
@@ -32,8 +35,6 @@ print("- ", len(realdataobj.index), " Real News")
 print("- ", len(fakedataobj.index), " Fake News")
 
 # VECTORISE COUNT OF WORDS IN TITLE COLUMN AND SCALE
-limitsamples = 5000   # KNN ENCOUNTERS MEMORY ERROR FOR TOO LARGE DATASETS
-xtrain, ytrain, xtest, ytest = xtrain[:limitsamples], ytrain[:limitsamples], xtest[:limitsamples], ytest[:limitsamples]
 countvec = CountVectorizer().fit(xtrain)      # only include words appearing in more than 5 titles
 xtrainvec = countvec.transform(xtrain)
 print("\n",repr(xtrainvec))
@@ -42,20 +43,19 @@ print(words[2000:2020])
 xtestvec = countvec.transform(xtest)
 
 # IMPLEMENT PURE ML MODEL LOGISTIC REGRESSION CLASSIFIER 
-score = cross_val_score(KNeighborsClassifier( n_jobs = -1), xtrainvec, ytrain, cv = 5, n_jobs = -1)
+score = cross_val_score(LogisticRegression(max_iter = 10000), xtrainvec, ytrain, cv = 5, n_jobs = 3)
 print("\nEach cross validated portion score array : ")
 print(score)
 print("Average CV Logistic regression training score : ", np.mean(score))
 
 # + GRIDSEARCH AND CROSS VALIDATION FOR PARAMETERS
 start = tm.time()
-param_grid = {'n_neighbors': [20, 40, 60, 80], 'weights': ["uniform", "distance"], 'algorithm': ['ball_tree', 'kd_tree', 'brute']}
-grid = GridSearchCV(KNeighborsClassifier(n_jobs = -1), param_grid, cv=5, n_jobs = -1)
+param_grid = {'C': [1, 2, 3, 4, 5], "solver": ["lbfgs", "saga", "sag", "liblinear"]}
+grid = GridSearchCV(LogisticRegression(max_iter = 10000, random_state = 8, n_jobs = -1), param_grid, cv=5, n_jobs = -1)
 grid.fit(xtrainvec, ytrain)
-print("\nTHE BEST OF K NEAREST NEIGHBOURS : ")
+print("\nTHE BEST OF LOGISTIC REGRESSION : ")
 print("Best cross-validation score: {:.2f}".format(grid.best_score_)) 
 print("Best parameters: ", grid.best_params_)
-print(grid.best_estimator_)
 
 # ACCURACY ON TRAIN AND TEST SET
 print("\nTraining Set Accuracy :")
@@ -63,11 +63,30 @@ print(round(grid.score(xtrainvec, ytrain), 4))
 print("Test Set Accuracy : ")
 print(round(grid.score(xtestvec, ytest), 4))
 
+# CONFUSION MATRIX FOR TRUE POSITIVES AND NEGATIVES
+print("Training set Confusion Matrix : ")
+cfmtrain = confusion_matrix(ytrain, grid.predict(xtrainvec))
+print(cfmtrain)
+print("True Positive: ", cfmtrain[1][1], "\nTrue Negative: ", cfmtrain[0][0], "\nFalse Positive: ", cfmtrain[0][1], "\nFalse Negative: ", cfmtrain[1][0])
+print()
+print("Test set Confusion Matrix : ")
+testpred = grid.predict(xtestvec)
+cfmtest = confusion_matrix(ytest, testpred)
+print(cfmtest)
+print("True Positive: ", cfmtest[1][1], "\nTrue Negative: ", cfmtest[0][0], "\nFalse Positive: ", cfmtest[0][1], "\nFalse Negative: ", cfmtest[1][0])
+print()
+print("General scores for confusion: ")
+print("Precision(Positivity rate): ", cfmtest[1][1]/(cfmtest[1][1] + cfmtest[0][1]))
+print("Recall(Positivity Error):", cfmtest[1][1]/(cfmtest[1][1] + cfmtest[1][0]))
+print("Classification Report: ")
+print(classification_report(ytest, testpred))
+
 # PREDICT THE NEWS STATUS OF A SAMPLE NEWS TITLE
 sometitle = "Donald Trump has hired Putin to be his wingman for destroying china"
 sometitlearray = np.array([[sometitle]])
 sometitlevec = countvec.transform(sometitlearray.ravel())
-print("Test Article :", sometitle)
+print("\nTest Article :", sometitle)
 print("You likely enterred a", grid.predict(sometitlevec)[0], "article")
 end = tm.time()
 print("\nTotal time to run :", round(end-start, 3), "seconds")
+
